7701417e8c4a676fea37953c014fe1435c20db1e
xnderLAN <ghilesmahleb@gmail.com>
2023-04-04 18:00:54 +0200

    update console

---
+++ src/console/tab.py
@@ -0,0 +1,22 @@
+tabs_in_mod = {
+    "Work":[
+        "mod insersion ",
+        "mod search ",
+        "help ",
+        "show mod ",
+        "show vul ",
+
+    ],
+    "Insert" : [
+        "new vul ",
+        "set dork",
+        "set vul ",
+        "insert"
+    ],
+    "Search" : [
+        "run ",
+        "set vul ",
+        "set dork"
+    ]
+
+}
--- src/console/workspace.py
+++ src/console/workspace.py
@@ -1,14 +1,19 @@
 import readline
 from colorama import Style
+from .tab import tabs_in_mod
+import sys
+from time import sleep
+
 
 class Interact():
     #intitle:"index of /" intext:".env"
 
     def __init__(self) -> None:
         self.mod = ['Work', 'Search', 'Insert']
-        self.in_mod = False
         self.current_mod = ''
         self.cmd_on_mod = []
+        self.tab_in_mod = tabs_in_mod
+        self.input = ""
         self.cmd = {
             'mod ':{
                 'insert ':{
@@ -29,6 +34,9 @@ class Interact():
             },
             'exit ':{
                 'discrib': ''
+            },
+            'show ':{
+                'discrib': ''
             }
         }
         
@@ -41,23 +49,36 @@ class Interact():
             return None
     
     def _tab_maker(self):
-        if self.in_mod :
-            self.cmd_on_mod = list(self.cmd['mod '][self.current_mod+" "].keys()).pop("discrib")
+        if self.current_mod :
+            self.cmd_on_mod = self.tab_in_mod[self.current_mod]
             return
         
         self.cmd_on_mod = self.cmd.keys()
 
     def console(self):
     
-        self.current_mod = self.mod[0]
+        self.current_mod = self.mod[0].strip(" ")
         while True:
             self._tab_maker()
             readline.set_completer(self._tab)
             readline.parse_and_bind('tab: complete')
-            commande = input(f'[{self.current_mod}] Doks >> ')
+            self.input = input(f'[{self.current_mod.capitalize()}] Doks >> ')
             # traitez la commande ici...
 
-            if commande.split(" ")[0] == "mod":
-                self.current_mod = commande.split(" ")[1]
+            if self.commande.split(" ")[0] == "mod":
+                self.current_mod = self.commande.split(" ")[1]
                 self.in_mod = True
+            
+            elif self.commande.split(" ")[0] == "show":
 
+                if self.current_mod ==  "work":
+                    pass
+                elif self.current_mod ==  "search":
+                    pass
+                elif self.current_mod ==  "insert":
+                    pass
+
+            elif self.commande.strip(" ") == "exit":
+                print("\nGoodbye !")
+                sleep(3)
+                sys.exit()
